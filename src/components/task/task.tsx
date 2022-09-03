416154facd1731d85ac88da33ceaae019b48d077
Armen Gulakhszyan <agulakhszyan@tradingview.com>
2022-09-03 12:24:08 +0300

    implement task and creation

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,5 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 
-export function Task(): JSX.Element {
-	return <div></div>;
+import { ApiCreateTaskPayload, ApiTaskStatus } from '../../api/api-objects-and-constants';
+
+import * as styles from './task.scss';
+
+export enum TaskActionType {
+	Create = 'Create',
+	Update = 'Update',
+}
+
+interface TaskProps {
+	actionType: TaskActionType;
+	action: (payload: ApiCreateTaskPayload) => Promise<void>;
+	title?: string;
+	status?: ApiTaskStatus;
+	description?: string;
+}
+
+export function Task(props: TaskProps): JSX.Element {
+	const [title, setTitle] = useState(props.title ?? '');
+	const [description, setDescription] = useState(props.description ?? '');
+	const [status, setStatus] = useState(props.status ?? ApiTaskStatus.Open);
+
+	return (
+		<div className={ styles.task }>
+			<input
+				type='text'
+				className={ classnames(styles.input, styles.title) }
+				placeholder='Title'
+				value={ title }
+				onChange={ handleTitleInput }
+			/>
+
+			<input
+				type='text'
+				className={ styles.input }
+				placeholder='Description'
+				value={ description }
+				onChange={ handleDescriptionInput }
+			/>
+
+			<select
+				className={ styles.status }
+				defaultValue={ status }
+				onChange={ handleStatusSelect }
+			>
+				<option value={ ApiTaskStatus.Open }>Open</option>
+				<option value={ ApiTaskStatus.Done }>Done</option>
+			</select>
+
+			<button
+				onClick={ handleCreate }
+			>
+				{props.actionType}
+			</button>
+		</div>
+	);
+
+	function handleTitleInput(event: React.ChangeEvent<HTMLInputElement>): void {
+		setTitle(event.target.value);
+	}
+
+	function handleDescriptionInput(event: React.ChangeEvent<HTMLInputElement>): void {
+		setDescription(event.target.value);
+	}
+
+	function handleStatusSelect(event: React.ChangeEvent<HTMLSelectElement>): void {
+		setStatus(event.target.value as ApiTaskStatus);
+	}
+
+	async function handleCreate(): Promise<void> {
+		await props.action({
+			title,
+			description,
+			status,
+		});
+
+		setTitle('');
+		setDescription('');
+		setStatus(ApiTaskStatus.Open);
+	}
 }
--- src/pages/tasks-page/tasks-page.tsx
+++ src/pages/tasks-page/tasks-page.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react';
 
+import { Task, TaskActionType } from '../../components/task/task';
 import { useObservable } from '../../hooks/use-observable';
 import { ITasksViewModel } from '../../models/tasks-view-model';
+
+import * as styles from './tasks-page.scss';
 
 interface TaskPageProps {
 	model: ITasksViewModel;
@@ -16,11 +19,26 @@ export function TasksPage(props: TaskPageProps): JSX.Element {
 
 	const tasks = useObservable(model.tasks$, model.tasks());
 
-	console.log(tasks);
+	const tasksContent = tasks.map((task) => {
+		return (
+			<Task
+				actionType={ TaskActionType.Update }
+				action={ async (): Promise<void> => { return; }}
+				title={ task.title }
+				description={ task.description }
+				status={ task.status }
+				key={ task._id }
+			/>
+		);
+	});
 
 	return (
-		<main>
-			Tasks
+		<main className={ styles.tasksPage }>
+			<div>
+				<h2>New task</h2>
+				<Task actionType={ TaskActionType.Create } action={ model.createTask } />
+			</div>
+			{ tasksContent }
 		</main>
 	);
 }
